e7dd67793aa0898de0341d7e6d8cfeb536f128af
899025 <899025@LTIN296872>
2021-07-19 17:50:53 +0530

    Add project files.

---
+++ Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.netCoreMVCCRUD.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int MovieId { get; set; }
+        [Required]
+        public string  MovieName { get; set; }
+        [Required]
+        public string ActorName { get; set; }
+        [Required]
+        public string  Category { get; set; }
+
+    }
+}
